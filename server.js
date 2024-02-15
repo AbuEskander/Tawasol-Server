47bffd1f1bda9b905ee52d55c3645e486e585742
Basil Ismail <basselawni1@gmail.com>
2024-02-15 15:08:27 +0300

    Fixed .env.PORT from .env.Process

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const App = express();
-const PORT = process.env.PROCESS || 5000;
+const PORT = process.env.PORT || 5000;
 
 App.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
