dd1cac82b18687ed8b3f27428e4add078f4c384b
Basil Ismail <basselawni1@gmail.com>
2024-02-17 17:17:57 +0300

    Done initializing

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
-
-const App = express();
+const connectDB = require("./config/db");
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-App.listen(PORT, () => {
+app.use("/api/users", require("./Routes/users"));
+app.use("/api/profiles", require("./Routes/profile"));
+app.use("/api/auth", require("./Routes/auth"));
+app.use("/api/posts", require("./Routes/posts"));
+
+connectDB();
+
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
