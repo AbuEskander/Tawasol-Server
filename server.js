b524591a9ab639f0881059d74cb491207dcb2c15
Basil Ismail <basselawni1@gmail.com>
2024-02-18 18:17:53 +0300

    Revert "Done initializing"
    This reverts commit dd1cac82b18687ed8b3f27428e4add078f4c384b.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const connectDB = require("./config/db");
-const app = express();
+
+const App = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
